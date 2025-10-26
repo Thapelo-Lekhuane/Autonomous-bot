bbd1c29ee308bbcdb66dc207036001ad0a144874
Thapelo Lekhuane <thapelolekhuane2@gmail.com>
2025-10-26 19:37:34 +0200

    logging to the try and catch for better debugging and early catching of errors

--- server/index.ts
+++ server/index.ts
@@ -13,8 +13,11 @@ const client = new MongoClient(process.env.MONGODB_ATLAS_URI as string);
 
 async function runServer() {
     try {
-
+        await client.connect();
+        await client.db('admin').command({ ping: 1 });
+        console.log('Connected successfully to MongoDB Atlas');
 
     } catch (error) {
         console.error(error);
     }
+}
