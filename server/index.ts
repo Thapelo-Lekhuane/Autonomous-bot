1cf344fb893e403396807f7cc7c6e909a2094989
Thapelo Lekhuane <thapelolekhuane2@gmail.com>
2025-10-26 20:08:23 +0200

    added post chat endpoint

--- server/index.ts
+++ server/index.ts
@@ -17,6 +17,41 @@ async function runServer() {
         await client.db('admin').command({ ping: 1 });
         console.log('Connected successfully to MongoDB Atlas');
 
+        app.get('/', (req: Request, res: Response) => {
+            res.send('LangGraph Agent Server');
+        })
+        app.post('/chat', async (req: Request, res: Response) => {
+            const initialMessage = req.body.message;
+            const threadId = Date.now().toString();
+            console.log(initialMessage)
+            try {
+                const response = await callAgent(client, initialMessage, threadId)
+                res.json({ threadId, response })
+            } catch (error) {
+               console.error('Error Starting Conversation:', error)
+                res.status(500).json({ error: 'Error Starting Conversation' })
+            }
+        })
+        app.post('/chat/:threadId', async (req: Request, res: Response) => {
+        const { threadId } = req.params;
+        connt { message } = req.body;  
+
+        try {
+            const response = await callAgent(client, message, threadId)
+           
+        } catch (error) {
+            console.error('Error Continuing Conversation:', error)
+        }
+         
+
+
+
+
+
+
+
+
+
     } catch (error) {
         console.error(error);
     }
